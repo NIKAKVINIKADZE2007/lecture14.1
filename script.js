e7712b318ae0c30e4de70a34497843572b3d419a
nikakvinikadze <nikakvinikadze2007@gmail.com>
2024-09-09 20:59:23 +0400

    destr

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('hello world');
+const alphabet = ['A', 'B', 'C', 'D', 'E'];
+const numbers = [1, 2, 3, 4, 5];
+
+const [first, second, third] = alphabet;
+
+const group3 = [...alphabet, ...numbers];
